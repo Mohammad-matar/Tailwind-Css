00021f04ab550b3bc2691156045d5f9cb10c8064
Mohammad Matar <moe.matar1998@gmail.com>
2022-05-18 12:27:05 +0300

    tailwindcss-test

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div className="flex justify-center bg-green-400 items-center	h-screen" >
+        <div className="bg-slate-800 p-12	rounded-lg">
+            <div className="p-4	text-7xl text-white	mb-20"> Wlc to TailwindCSS</div> 
+            
+            <form className="flex flex-col text-white">
+                <input className="bg-slate-200	text-black w-96 m-3	p-4 ml-40 text-lg" type="text" placeholder="Username"/>
+                <input className="bg-slate-200	text-black w-96 m-3	p-4	ml-40 text-lg" type="password" placeholder="Password"/>
+            </form>
+             <button type="button" className="bg-white text-black ml-80 p-4 pl-5 rounded-full	hover:bg-sky-700 text-lg mt-5" >Login!</button>
+
+    
+        </div>
     </div>
+    
+
   );
 }
 
